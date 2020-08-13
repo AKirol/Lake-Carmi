b38a3d701c59f9667a4b4d0d999c7ca934f36d69
Patrick Clemins <patrick.clemins@uvm.edu>
2020-08-13 19:31:09 -0400

    Update rpubs publishing script for Ashton

--- scripts/LakeCarmiRPubsUpdateForAshton.R
+++ scripts/LakeCarmiRPubsUpdateForAshton.R
@@ -4,6 +4,6 @@ library(rmarkdown)
 ## Don't forget to setwd('stuff') to where your CarmiSite.Rmd file is!!
 
 render("./CarmiSite.Rmd",output_file="./LakeCarmiData.html")
-rpubsUpload("UVM Lake Carmi Platform Data", htmlFile="./LakeCarmiData.html", id="https://api.rpubs.com/api/v1/document/644651/30cef508876a47dda2da12c8759093fc")
+rpubsUpload("UVM Lake Carmi Platform Data", htmlFile="./LakeCarmiData.html", id="https://api.rpubs.com/api/v1/document/648834/6e16202f48734b3682e5b3e1dca9c099"))
 
 unlink("./LakeCarmiData.html")
